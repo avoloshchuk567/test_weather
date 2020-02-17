98deff5e206ff1a09d4d8e6fb7f5a1757184b0ab
Alla Voloshchuk <avoloshchuk@griddynamics.com>
2020-02-17 17:26:16 +0200

    Logging has been added

--- src/test/java/weatherApiTests/AuthKeyTest.java
+++ src/test/java/weatherApiTests/AuthKeyTest.java
@@ -1,48 +1,63 @@
 package weatherApiTests;
 
 import Utils.EndPoints;
+import io.restassured.response.Response;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class AuthKeyTest {
     private EndPoints endPoint1 = new EndPoints();
 
+    private static final Logger LOG = LoggerFactory.getLogger(AuthKeyTest.class);
+    private static int expectedStatusCodeUnath = 401;
+    private static int expectedStatusCodeOk = 200;
+    private static String requestParamBy = "q";
+    private static String requestParamByValue = "London";
+    private static String requestParamAuth = "APPID";
+
+
+    @Test(groups = {"functional"})
+    public void withoutAuthParam() {
+        LOG.debug("Authentication without API Key tests");
+        given().spec(endPoint1.getBasePath()
+                .addParam(requestParamBy, requestParamByValue)
+                .build())
+                .when()
+                .get()
+                .then()
+                .statusCode(expectedStatusCodeUnath);
+
+        LOG.debug("Test: It's impossible to get data without APPID");
+    }
+
     @DataProvider
-    @Parameters({"statusCodeUnath", "statusCodeOk"})
-    public Object[][] requestData(int statusCodeUnath, int statusCodeOk) {
+    public Object[][] requestData() {
         return new Object[][]{
-                {null, statusCodeUnath, "verifyNullAppId"},
-                {"4t5gdg534df453", statusCodeUnath, "verifyIncorrectAppIdShorl"},
-                {"0b4a9150dfd5e34fc0d8afa2a8d1629f", statusCodeUnath,"verifyIncorrectAppIdFull"},
-                {"0b4a9150ecd5efe7c0d8afa2a8d1629e", statusCodeOk, "verifyValidAppId"}
+                {null, expectedStatusCodeUnath, "verifyNullAppId"},
+                {"4t5gdg534df453", expectedStatusCodeUnath, "verifyIncorrectAppIdShorl"},
+                {"0b4a9150dfd5e34fc0d8afa2a8d1629f", expectedStatusCodeUnath, "verifyIncorrectAppIdFull"},
+                {"0b4a9150ecd5efe7c0d8afa2a8d1629e", expectedStatusCodeOk, "verifyValidAppId"}
 
         };
     }
 
     @Test(dataProvider = "requestData", groups = {"smoke", "functional"}, dependsOnMethods = {"withoutAuthParam"})
-    public void testAuthKey(String appId, int statusCode, String testDataDescription) {
-        given().spec(endPoint1.getBasePath()
-                .addParam("q", "London")
-                .addParam("APPID", appId)
+    public void testAuthKey(String appId, int expectedStatusCode, String testDataDescription) {
+        LOG.debug("Authentication with incorrect and valid API Keys");
+        Response wholeResponse = given().spec(endPoint1.getBasePath()
+                .addParam(requestParamBy, requestParamByValue)
+                .addParam(requestParamAuth, appId)
                 .build())
                 .when()
-                .get()
-                .then()
-                .statusCode(statusCode);
-    }
-
-    @Test(groups = {"functional"})
-    @Parameters("statusCodeUnath")
-    public void withoutAuthParam(int statusCodeUnath) {
-        given().spec(endPoint1.getBasePath()
-                .addParam("q", "London")
-                .build())
-                .when()
-                .get()
-                .then()
-                .statusCode(statusCodeUnath);
+                .get();
+        LOG.debug("Test: {}, expected value: {} = {} ", testDataDescription, "expectedStatus code", expectedStatusCode);
+        LOG.debug("Request address: {}", wholeResponse.getHeaders().getValue("X-Cache-Key"));
+        assertThat(wholeResponse.getStatusCode(), equalTo(expectedStatusCode));
     }
 }
--- src/test/java/weatherApiTests/ResponseParametersTest.java
+++ src/test/java/weatherApiTests/ResponseParametersTest.java
@@ -1,14 +1,18 @@
 package weatherApiTests;
 
 import Utils.EndPoints;
+import io.restassured.response.Response;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class ResponseParametersTest {
+    static final Logger LOG = LoggerFactory.getLogger(ResponseParametersTest.class);
     EndPoints endPoint1 = new EndPoints();
     private static final String AUTHAPPID = "APPID";
     private static final String APIKEY = "0b4a9150ecd5efe7c0d8afa2a8d1629e";
@@ -29,14 +33,19 @@ public class ResponseParametersTest {
     public void responseCoordinatesTest(String requestId, String requestIdValue,
                                         String responseParam, String responseParamExpectedValue,
                                         String testDataDescription) {
-
-        String responseParamActualValue = (given().spec(endPoint1.getBasePath()
+        LOG.debug("Verification of the parameters in response");
+        Response wholeResponse = (given().spec(endPoint1.getBasePath()
                 .addParam(requestId, requestIdValue)
                 .addParam(AUTHAPPID, APIKEY)
                 .build())
                 .when()
-                .get())
-                .getBody()
+                .get());
+        LOG.debug("Test: {}, expected parameter: {} = {} ", testDataDescription, responseParam, responseParamExpectedValue);
+        LOG.debug("Request address: {}", wholeResponse.getHeaders().getValue("X-Cache-Key"));
+        LOG.debug("Response body: {}", wholeResponse.getBody().asString());
+
+
+        String responseParamActualValue = wholeResponse.getBody()
                 .jsonPath()
                 .get(responseParam)
                 .toString();
